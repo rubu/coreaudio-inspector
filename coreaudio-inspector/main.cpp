5948eb17bb37b24baa2f26c5b10a67d8438cc75f
Rudolfs Bundulis <rudolfsbundulis@rudolfss-mac-mini.local>
2017-06-29 22:41:47 +0300

    Initial Commit

---
+++ coreaudio-inspector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coreaudio-inspector
+//
+//  Created by Rudolfs Bundulis on 29/06/17.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
